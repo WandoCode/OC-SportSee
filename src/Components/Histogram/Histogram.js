ba16ed814d2b0358fed016ddae2f23bdbbe53de4
Wandole <mchirez@gmail.com>
2022-09-19 19:46:46 +0200

    Make histogram responsive

--- src/Components/Histogram/Histogram.js
+++ src/Components/Histogram/Histogram.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from 'react'
-import { BarChart, Bar, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts'
+import {
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts'
 import userStore from '../../store/userStore'
 import './histogram.css'
 
@@ -27,58 +35,58 @@ function Histogram({ userId }) {
 
   return (
     <div className="histogram">
-      <BarChart
-        width={750}
-        height={230}
-        data={userActivity}
-        margin={{
-          top: 50,
-          right: 0,
-          left: 31,
-          bottom: 5,
-        }}
-      >
-        <CartesianGrid strokeDasharray="1 4" vertical={false} />
-        <YAxis
-          dataKey="kilogram"
-          tickCount={3}
-          yAxisId="kilogram"
-          orientation="right"
-          axisLine={false}
-          tickMargin={30}
-          tickLine={false}
-        />
+      <ResponsiveContainer width="100%" height="100%">
+        <BarChart
+          data={userActivity}
+          margin={{
+            top: 50,
+            right: 0,
+            left: 31,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="1 4" vertical={false} />
+          <YAxis
+            dataKey="kilogram"
+            tickCount={3}
+            yAxisId="kilogram"
+            orientation="right"
+            axisLine={false}
+            tickMargin={30}
+            tickLine={false}
+          />
 
-        <YAxis
-          dataKey="calories"
-          hide={true}
-          domain={domainCalories}
-          yAxisId="calories"
-        />
+          <YAxis
+            dataKey="calories"
+            hide={true}
+            domain={domainCalories}
+            yAxisId="calories"
+          />
 
-        <XAxis
-          dataKey="day"
-          padding={{ left: -27, right: -27 }}
-          tickMargin={16}
-          tickLine={false}
-        />
-        <Tooltip />
+          <XAxis
+            dataKey="day"
+            padding={{ left: -27, right: -27 }}
+            tickMargin={16}
+            tickLine={false}
+          />
+          <Tooltip />
 
-        <Bar
-          dataKey="kilogram"
-          className="kilogram"
-          yAxisId="kilogram"
-          barSize={7}
-          radius={[3.5, 3.5, 0, 0]}
-        />
-        <Bar
-          dataKey="calories"
-          className="calories"
-          yAxisId="calories"
-          barSize={7}
-          radius={[3.5, 3.5, 0, 0]}
-        />
-      </BarChart>
+          <Bar
+            dataKey="kilogram"
+            className="kilogram"
+            yAxisId="kilogram"
+            barSize={7}
+            radius={[3.5, 3.5, 0, 0]}
+          />
+          <Bar
+            dataKey="calories"
+            className="calories"
+            yAxisId="calories"
+            barSize={7}
+            radius={[3.5, 3.5, 0, 0]}
+          />
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   )
 }
